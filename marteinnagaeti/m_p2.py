7ee1ee56b000890a77bc8a475077a2b5efe660c7
marteinn_vidir <marteinns19@ru.is>
2022-11-29 11:40:04 +0000

    yeah

--- marteinnagaeti/m_p2.py
+++ marteinnagaeti/m_p2.py
@@ -1,27 +1,69 @@
 import numpy as np
-from numpy import linalg as LA
 import matplotlib.pyplot as plt
-
-
+from matplotlib.animation import PillowWriter
+from matplotlib import animation
 g = 9.81
-L = 2
+def prob1():
+    pass
 
-def prob1(L,y):
-    return np.array([y[1],-(L/g)*np.sin(y[0])])
+def prob2F(theta):
+    L = 2
+    z1 = theta[1,0]
+    z2 = (-g/L)*np.sin(theta[0,0])
+    return np.matrix([[z1],[z2]])
 
-
-def Euler_step(initalval, T, n):
+def euler(n, T, initalValues, F):
     h = T/n
-    theta = np.zeros((n,2))
-    theta[0] = initalval
-    for i in range(1,n):
-      theta[i] =  theta[i-1] + h*prob1(L, theta[i-1])
-    return theta
+    theta = np.zeros((initalValues.size, n+1))
+    theta[:,0:1] = initalValues
+    for i in range(n):
+        theta[:,(i+1):(i+2)] = theta[:,i:(i+1)] + F(theta[:,i:(i+1)])*h
     
+    return theta
+
+def prob2():
+    prob2InitialVal = np.matrix([[np.pi/12], [0]])
+    theta = euler(200, 5, prob2InitialVal, prob2F)
+    print(theta)
+
+
+
+
 
     
+
+def animatePendulum(theta):
+    def animate(i):
+        line.set_data([0,x[i]], [0,y[i]])
+        ln1.set_data([x[i]], [y[i]])
+
+        
+    L = 2
+    print(theta.shape)
+    x = np.cos(theta[0]-(np.pi/2)) * L
+    y = np.sin(theta[0]-(np.pi/2)) * L
+    
+    print("x:{}\ny:{}".format(x, y))
+    fig,ax = plt.subplots(1,1)
+    ax.grid()
+    ln1, = plt.plot([], [], 'ro', markersize=10)
+    line, = plt.plot([], [], 'b-', linewidth=3)
+    ax.set_ylim(-2.5, 2.5)
+    ax.set_xlim(-2.5, 2.5)
+    ani = animation.FuncAnimation(fig, animate, frames=500, interval=20)
+    ani.save('pen.gif', writer='pillow', fps=25)
+    print("done")
+
+def prob3():
+    prob3InitialVal = np.matrix([[np.pi/12], [0]])
+    theta = euler(500, 20, prob3InitialVal, prob2F)
+    print(theta[:,500])
+    animatePendulum(theta)
+
+
+    
+
 
 if __name__ == "__main__":
-    a = Euler_step([np.pi/12, 0], 20, 500)
-    for i in range(len(a)):
-        print(a[i])
+    # prob2()
+    prob3()
