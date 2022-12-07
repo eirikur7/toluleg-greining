563f783f89a736e400503c216df01299436c4df8
Eirikur Bernharðsson <eirikurb19@ru.is>
2022-12-07 16:33:59 +0000

    a;lsdkjfasd;lkfj;alsdkfj;asldkfjk;alsdskjf;al

---
+++ src/Project3.py
@@ -0,0 +1,25 @@
+
+
+def prob1():
+    print('--- Problem 1 ---')
+    
+def prob2():
+    print('--- Problem 2 ---')
+    print('Not implemented')
+
+def prob3():
+    print('--- Problem 3 ---')
+    print('Not implemented')
+
+def prob4():
+    print('--- Problem 4 ---')
+    print('Not implemented')
+
+def main():
+    prob1()
+    prob2()
+    prob3()
+    prob4()
+
+if __name__ == '__main__':
+    main()
